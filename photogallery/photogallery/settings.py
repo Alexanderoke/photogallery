ea26a7934081177a61f608c02bbdd00ddd0c78cb
alexanderoke <alexander.oke@student.moringaschool.com>
2022-02-27 08:38:19 +0300

    create a function for rendering home.html

--- photogallery/photogallery/settings.py
+++ photogallery/photogallery/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'picgallery.apps.PicgalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- photogallery/picgallery/views.py
+++ photogallery/picgallery/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+'''
+home route function
+'''
+def home(request):
+  return render(request,'picgallery/home.html')
