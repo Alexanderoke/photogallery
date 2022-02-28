1a9c924d485c86c594b8a81f0c10dbf7951a380e
alexanderoke <alexander.oke@student.moringaschool.com>
2022-02-28 18:41:42 +0300

    add view picture url to the view photo route

--- photogallery/picgallery/urls.py
+++ photogallery/picgallery/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.gallery, name='picgallery-gallery'),
 
-    path('<str:pk>', views.view_photo, name='picgallery-view_photo'),
+    path('picgallery/<str:pk>/', views.view_photo, name='picgallery-view_photo'),
     
     path('addpics/', views.addpics, name='picgallery-addpics'),
     
