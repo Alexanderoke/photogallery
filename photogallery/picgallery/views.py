a81d1e01ac68faaeda0f842c417dd23740db4ed7
alexanderoke <alexander.oke@student.moringaschool.com>
2022-03-01 11:17:36 +0300

    add a csrf token to addpics template form and add if conditional for creating a new category on the addpics view function

--- photogallery/picgallery/views.py
+++ photogallery/picgallery/views.py
@@ -31,5 +31,23 @@ add pictures route function
 def addpics(request):
   categories = Category.objects.all()
 
+  if request.method == 'POST':
+    data = request.POST
+    image = request.FILES.get('image')
+
+    if data['category'] != 'none':
+      category =Category.objects.get(id=data['category'])
+    elif data['category_new'] !='':
+      category, created =Category.objects.get_or_create(name=data['category_new'])
+    else:
+      category= None
+
+      picture =Picture.objects.create(
+        category=category,
+        description=data['description'],
+        image=image
+
+      )
+
   context ={'title':'Add Pictures' ,'categories':categories}
   return render(request,'picgallery/addpics.html',context)
